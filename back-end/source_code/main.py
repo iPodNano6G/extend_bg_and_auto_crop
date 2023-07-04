e97fd734d9fbc2e3d6724aa7b5213c2d92bf8a08
HyeonSeok Sang <henry914@ajou.ac.kr>
2023-07-04 09:18:40 +0900

    json 문서를 생성하고 저장하는 클래스를 추가했습니다.

--- back-end/source_code/main.py
+++ back-end/source_code/main.py
@@ -10,6 +10,9 @@ from modules import photoshop
 with open(sys.argv[1], 'r', encoding='UTF8') as file:
     shoppingmall_data = json.load(file)
 
+
+boxDrower = object_detector.BoxDrower()
+
 seller_brand_path = shoppingmall_data["seller"]+"/"+shoppingmall_data["brand"]
 
 imageDownloder = fileManager.ImageDownloader()
@@ -20,9 +23,11 @@ imageProcessor = image_processor.ImageProcessor()
 faceDetector = object_detector.FaceDetector()
 clothesDetector = object_detector.ClothesDetector()
 
-databaseManager = database.DatabaseManager()
 
-photoshop_ = photoshop.Photoshop()
+jsonMaker = fileManager.JsonMaker()
+DocumentManager = fileManager.DocumentManager()
+
+photoshop_ = photoshop.Photoshop("C:/Program Files/Adobe/Adobe Photoshop 2023/Photoshop.exe")
 
 for product in shoppingmall_data["product_list"]:
     save_path = "image_data/"+seller_brand_path+"/"+product["name"]
@@ -30,15 +35,25 @@ for product in shoppingmall_data["product_list"]:
     image_path = imageDownloder.download_image(image_url, save_path, "original.jpg")
     image = imageFactory.create_image(image_path, None, image_url)
 
+
     result_image = imageProcessor.process(image)
+    boxDrower.drawBox(result_image, (result_image.x_offset, result_image.y_offset), (result_image.x_offset + image.width, result_image.y_offset + image.height), "box_result_box")
+    print((result_image.x_offset, result_image.y_offset), (result_image.x_offset + image.width, result_image.y_offset + image.height))
 
     faceData = faceDetector.detectFace(image)
     face = faceData.getFace(1)
+    print((face.left, face.top), (face.right, face.bottom))
+    boxDrower.drawBox(image, (face.left, face.top), (face.right, face.bottom), "box_face")
     
+
     clothesDataList = clothesDetector.localize_objects(image)
     clothes = clothesDataList.getClothes(product["type_of_clothes"])
     clothes.denormalizeByImageSize(image)
+    boxDrower.drawBox(image, (clothes.left, clothes.top), (clothes.right, clothes.bottom), "box_clothes")
 
-    no_background_image_path = photoshop_.backgroundRemover(image_path)
+    no_background_image_path = photoshop_.remove_background(image_path)
 
-    databaseManager.insertToClothesDataCollection(image, image_url, result_image, face, clothes)
+    json_document = jsonMaker.make_json_for_clothes_data(image, image_url, result_image, face, clothes, no_background_image_path)
+    json_path = DocumentManager.save_json_document(json_document, image_path)
+
+    #databaseManager.insertToClothesDataCollection(image, image_url, result_image, face, clothes)
--- back-end/source_code/modules/fileManager.py
+++ back-end/source_code/modules/fileManager.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import json
+
 class ImageDownloader:
     def download_image(self, url, folder_path, file_name):
         response = requests.get(url)
@@ -10,3 +12,46 @@ class ImageDownloader:
                 file.write(response.content)
             return os.path.normpath(os.path.abspath(image_path))
 
+class DocumentManager:
+    def save_json_document(self, json_document, file_path):
+        abs_path = os.path.abspath(file_path)
+        absolute_folder_path = os.path.dirname(abs_path)
+        # BSON으로 직렬화된 데이터를 파일에 저장
+        json_path = absolute_folder_path+"\\data.json"
+        with open(json_path, 'w', encoding='utf-8') as file:
+            json.dump(json_document, file, ensure_ascii=False)
+        return json_path
+class JsonMaker:
+    def make_json_for_clothes_data(self, orignalImage, imaga_url, finalImage, face, clothes, no_background_image_path):
+        json_document = {
+            "original_image": {
+                "file_path": orignalImage.filepath,
+                "url": imaga_url,
+                "width": orignalImage.width,
+                "height": orignalImage.height
+            },
+            "dalle_image": {
+                "file_path": finalImage.filepath,
+                "dalle_image_width": finalImage.width,
+                "dalle_image_height": finalImage.height,
+                "x_offset": finalImage.x_offset,
+                "y_offset": finalImage.y_offset,
+            },
+            "face": {
+                "left_line_pixel": face.left + finalImage.x_offset,
+                "right_line_pixel": face.right + finalImage.x_offset,
+                "top_line_pixel": face.top + finalImage.y_offset,
+                "bottom_line_pixel": face.bottom + finalImage.y_offset,
+            },
+            "clothes": {
+                "type_of_clothes": clothes.type_of_clothes,
+                "left_line_pixel": clothes.left + finalImage.x_offset,
+                "right_line_pixel": clothes.right + finalImage.x_offset,
+                "top_line_pixel": clothes.top + finalImage.y_offset,
+                "bottom_line_pixel": clothes.bottom + finalImage.y_offset,
+            },
+            "no_background": {
+                "file_path": no_background_image_path.replace("\\\\", "\\")
+            }
+        }
+        return json_document
