c9a81a6e662395d872b160b8390679c455db9e78
HyeonSeok Sang <henry914@ajou.ac.kr>
2023-07-04 09:15:45 +0900

    파라미터로 생성된 bson문서를 전달받도록 수정했습니다.

--- back-end/source_code/modules/database.py
+++ back-end/source_code/modules/database.py
@@ -10,34 +10,8 @@ class DatabaseManager:
             return True  # 실행 중인 경우 True 반환
         except:
             return False
-    def insertToClothesDataCollection(self, orignalImage, imaga_url, finalImage, face, clothes):
+    def insertToClothesDataCollection(self, bson_document):
         db = self.client['fashionImageTest']
         collection = db['clothesData']
-        inserted_data = {
-            "original_image": {
-                "file_path": orignalImage.filepath,
-                "url": imaga_url
-            },
-            "dalle_image_path": finalImage.filepath,
-            "dalle_image_width": finalImage.width,
-            "dalle_image_height": finalImage.height,
-            "x_offset": finalImage.x_offset,
-            "y_offset": finalImage.y_offset,
-            "face": {
-                "left_line_pixel": face.left + finalImage.x_offset,
-                "right_line_pixel": face.right + finalImage.x_offset,
-                "top_line_pixel": face.top + finalImage.y_offset,
-                "bottom_line_pixel": face.bottom + finalImage.y_offset,
-            },
-            "clothes": {
-                "type_of_clothes": clothes.type_of_clothes,
-                "left_line_pixel": clothes.left + finalImage.x_offset,
-                "right_line_pixel": clothes.right + finalImage.x_offset,
-                "top_line_pixel": clothes.top + finalImage.y_offset,
-                "bottom_line_pixel": clothes.bottom + finalImage.y_offset,
-            }
-        }
-        print(type(inserted_data["clothes"]["left_line_pixel"]))
-        print(inserted_data["clothes"]["left_line_pixel"])
-        insert_result = collection.insert_one(inserted_data)
+        insert_result = collection.insert_one(bson_document)
         print(f"Inserted document ID: {insert_result.inserted_id}")
