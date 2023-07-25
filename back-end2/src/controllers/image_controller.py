046f3b67863a0fd10a845312bd95e259a5fe9640
HyeonSeok Sang <henry914@ajou.ac.kr>
2023-07-26 07:17:08 +0900

    이미지 관련 컨트롤러 추가

---
+++ back-end2/src/controllers/image_controller.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request, jsonify
+
+from services.aggregated_services.image_processing_service import ImageProcessingService
+from utils.image.image_converter import ImageConverter
+
+image_controller  = Blueprint('image', __name__)
+
+@image_controller.route('/ProcessImage', methods=['POST'])
+def process_image():
+    ratio = int(request.args.get('ratio', 50))  # ratio 파라미터가 없으면 기본값 50 사용
+    recover_size = request.args.get('recover_size', 'True').lower() == 'true'
+    chop_image = request.args.get('chop_image', 'True').lower() == 'true'
+    outpainting_size = int(request.args.get('outpainting_size', 1024))
+    removed_border_pixel = int(request.args.get('removed_border_pixel', 2))
+    image_bytes = request.files['image_data'].read()
+    np_image = ImageConverter().convert_to_np_image(image_bytes)
+    processed_np_image = ImageProcessingService().process(np_image, ratio, recover_size, chop_image, outpainting_size, removed_border_pixel)
