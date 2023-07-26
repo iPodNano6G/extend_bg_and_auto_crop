94f93a8d9fe131d40614bb2b54fdd26c50c74929
HyeonSeok Sang <henry914@ajou.ac.kr>
2023-07-26 11:39:44 +0900

    minor update

--- image_processing_app/src/config.py
+++ image_processing_app/src/config.py
@@ -1,12 +1,5 @@
-import json
+from dotenv import load_dotenv
+import os
 
-
-def load_config(file_path):
-    with open(file_path, 'r') as file:
-        config_data = json.load(file)
-    return config_data
-
-config = load_config('../conf/config.json')
-
-unet_model_location = config["unet_model_location"]
-dallE_api_key = config["dallE_api_key"]
+unet_model_location = os.getenv('UNET_MODEL_LOCATION')
+dallE_api_key = os.getenv('DallE_API_KEY')
