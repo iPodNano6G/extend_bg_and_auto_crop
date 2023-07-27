1027d9f745aa1efb481166061c4fd59cfcb00189
HyeonSeok Sang <henry914@ajou.ac.kr>
2023-07-28 03:45:19 +0900

    자동 Crop Spring App 생성

---
+++ auto_crop_app/src/main/java/com/hyeonseok/auto_crop_app/AutoCropAppApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonseok.auto_crop_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCropAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCropAppApplication.class, args);
+	}
+
+}
---
+++ auto_crop_app/src/test/java/com/hyeonseok/auto_crop_app/AutoCropAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonseok.auto_crop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCropAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
