a731aa62b1a3341c2bc6aab97f41583dcd7a96c6
HyeonSeok Sang <henry914@ajou.ac.kr>
2023-07-28 00:42:32 +0900

    onnxruntime를 더 이상 사용하지 않도록 코드 MaskDetector 클래스 수정 기존에는 onnxruntime으로 가중치 파일을 직접 로드하고, 이를 이용해 피사체에 해당하는 부분의 마스크를 얻었습니다. 이 방식이 나쁜 것은 아닙니다. 하지만 rembg 라이브러리를 사용해 배경을 제거하고, 비투명 픽셀을 객체의 마스크로 변환하면, 코드가 훨씬 간결해집니다. 또한 onnxruntime을 사용할 경우, 가중치 파일을 직접 관리해야 하기에, 불필요한 업무가 추가된다고 판단하여, 수정했습니다

--- image_processing_app/src/utils/masks/mask_detector.py
+++ image_processing_app/src/utils/masks/mask_detector.py
@@ -2,8 +2,13 @@ import onnxruntime
 import cv2
 import numpy as np
 
+from utils.image.image_bg_remover import ImageBgRemover
+
 class MaskDetector:
     def detect_mask(self, np_image):
+        bg_removed_np_image = ImageBgRemover.remove_background(np_image)
+        mask = np.where(bg_removed_np_image[..., 3] == 0, 0, 255).astype(np.uint8)
+        """
         model = onnxruntime.InferenceSession("../static/model_weight/unet.onnx")
         mask = cv2.resize(np_image, (320, 320))
         mask = mask.transpose((2, 0, 1))  # 채널 순서 변경
@@ -17,5 +22,6 @@ class MaskDetector:
         mask = mask[0, 0, :, :]  # 배치와 채널 차원 제거
         mask = cv2.resize(mask, (np_image.shape[1], np_image.shape[0]))  # 원래 크기로 복원. 이 마스크는 확장 영역을 선택할 때 쓰임.
         mask = (mask > 0.5).astype(np.uint8) * 255  # 이진화
-
+        """     
+        
         return mask
